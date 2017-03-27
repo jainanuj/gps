96561579d390e874d6e5190927d0eb14f7f69b0d
Anuj Jain <anujjain@adobe.com>
2017-03-27 17:56:51 -0600

    Adding the createGrid program and ignore mdp part logs and resuts files

---
+++ generateGrid/createGrid.h
@@ -0,0 +1,31 @@
+//
+//  createGrid.h
+//  gps-generate-xcode
+//
+//  Created by Anuj Jain on 3/27/17.
+//  Copyright © 2017 Anuj Jain. All rights reserved.
+//
+
+#ifndef createGrid_h
+#define createGrid_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//#define DIMENSIONS 2
+
+#define MAX_X 3
+#define MAX_Y 3
+
+#define START_X  0
+#define START_Y  0
+
+#define END_X    2
+#define END_Y    2
+int maxX = MAX_X;
+int maxY = MAX_Y;
+
+int endX = END_X;
+int endY = END_Y;
+
+#endif /* createGrid_h */
